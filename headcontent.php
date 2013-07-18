301c66818d0728f3b12cbf999323c878a76f8051
Kenneth Young <kly411@gmail.com>
2013-07-18 06:10:37 -0500

    third commit after single file edit

--- headcontent.php
+++ headcontent.php
@@ -3,7 +3,7 @@
 	<meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
 	<META HTTP-EQUIV='EXPIRES' CONTENT='0' />
 	<META NAME ='keywords' CONTENT='homes for sale benton arkansas, homes for sale bryant arkansas, new homes bryant arkansas, new homes benton arkansas, homes, home seller, houses benton arkansas, homes bryant arkansas, ken young, ken young company, real estate benton arkansas, real estate bryant arkansas'>
-
+    <!-- test -->
 	<script type='text/javascript' src='script/jquery-1.7.1.min.js'></script>
 	<script type='text/javascript' src='script/jquery-ui-1.8.22.custom/js/jquery-ui-1.8.22.custom.min.js'></script>
 	<script type='text/javascript' src='script/jquery.validate.min.js'></script>
